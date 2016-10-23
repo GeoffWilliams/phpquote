76d88f865dc7fa730bf1147ac59dbee1ff55828d
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-23 15:44:19 -0700

    works with docker compose for easy testing

--- quote/res/sql/schema.sql
+++ quote/res/sql/schema.sql
@@ -1,16 +1,11 @@
-CREATE TABLE IF NOT EXISTS quote (
+CREATE TABLE IF NOT EXISTS quote.quote (
   id MEDIUMINT NOT NULL AUTO_INCREMENT,
   quote VARCHAR(255) NOT NULL,
   author VARCHAR(255) NOT NULL,
   ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
---  id  INTEGER NOT NULL AUTOINCREMENT
---    author      VARCHAR(255),
---    quote       VARCHAR(255)
---);
 
-
-INSERT INTO quote (author, quote) VALUES ('Legendary', 'You\'ve got to speculate to accumulate');
-INSERT INTO quote (author, quote) VALUES ('Yoda', 'Do. Or do not. There is no try.');
+INSERT INTO quote.quote (author, quote) VALUES ('Legendary', 'You\'ve got to speculate to accumulate');
+INSERT INTO quote.quote.quote (author, quote) VALUES ('Yoda', 'Do. Or do not. There is no try.');
 
