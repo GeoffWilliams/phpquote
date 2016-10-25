3403b2a3dbb9a4e5dfddd609335d7216813a681f
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-25 13:58:38 -0700

    remove crud

--- res/config/config.php
+++ res/config/config.php
@@ -1,11 +1,4 @@
 <?php
 
 
-$db_options = [];
-$db_options['db.options'] = array(
-  "driver" => "pdo_mysql",
-  "user" => getenv('MYSQL_DB_USER'),
-  "password" => getenv('MYSQL_DB_PASSWORD'),
-  "dbname" => "quote",
-  "host" => getenv('MYSQL_DB_HOST'),
 ?>
